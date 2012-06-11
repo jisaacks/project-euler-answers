4e365fbda081522a318e1b1f32bbd1aa520c46e4
John Isaacks <john.isaacks@programming-perils.com>
2012-06-11 00:00:31 -0400

    The algorithm produces the correct number but isn't efficient enough

---
+++ 3.rb
@@ -0,0 +1,12 @@
+require "prime"
+
+$highest = 2
+
+def highest_prime_facter(n)
+  half = (n/2).round
+  facters = (1..half).select { |i| n % i == 0 }
+  primes = facters.select{ |i| Prime.prime? i }
+  primes.last
+end
+
+puts highest_prime_facter(600851475143)
