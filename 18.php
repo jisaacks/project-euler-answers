38d020391ef609b96e7c1651840456b68e95da65
John Isaacks <john.isaacks@programming-perils.com>
2012-06-24 23:25:02 -0400

    Add problem decsriptions

--- 18.php
+++ 18.php
@@ -1,21 +1,34 @@
 <?php
 
-/**
- * This took me a while to figure out. I was going about it the wrong way at
- * first. I was trying to go through all possible paths one at a time and I
- * couldn't figure a good solution to make sure I went through all the paths
- * possible without going through the same path twice. Then I thought, I
- * should go through all the possible paths concurrently.
- *
- * At the top of the pyramid working your way down you will always have 2
- * adjacent options, below to the left and below to the right. So I split
- * the process everytime trying both left and right recursively until I
- * have reached the bottom of the pyramid, I then take the value and add it
- * to the $ends array as a possible value. Once I have gone through all the
- * possible paths the $ends array will contain all the possible totals for
- * each path. Then to get the highest I just sort the array and grab the last
- * value.
- */
+// By starting at the top of the triangle below and moving to 
+// adjacent numbers on the row below, the maximum total from 
+// top to bottom is 23.
+
+// 3
+// 7 4
+// 2 4 6
+// 8 5 9 3
+
+// That is, 3 + 7 + 4 + 9 = 23.
+
+// Find the maximum total from top to bottom of the triangle below:
+
+// 75
+// 95 64
+// 17 47 82
+// 18 35 87 10
+// 20 04 82 47 65
+// 19 01 23 75 03 34
+// 88 02 77 73 07 63 67
+// 99 65 04 28 06 16 70 92
+// 41 41 26 56 83 40 80 70 33
+// 41 48 72 33 47 32 37 16 94 29
+// 53 71 44 65 25 43 91 52 97 51 14
+// 70 11 33 28 77 73 17 78 39 68 17 57
+// 91 71 52 38 17 14 91 43 58 50 27 29 48
+// 63 66 04 68 89 53 67 30 73 16 69 87 40 31
+// 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
 
 class PyramidMax
 {
