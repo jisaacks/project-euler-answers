1a700e8482ac1ceb18fbdb2c6e207ad55177e157
JD Isaacks <jd@jisaacks.com>
2013-07-16 21:30:25 -0400

    Fix 14

--- 14.hs
+++ 14.hs
@@ -1,35 +1,45 @@
+-- The following iterative sequence is defined for the set of positive integers:
+
+-- n  n/2 (n is even)
+-- n  3n + 1 (n is odd)
+
+-- Using the rule above and starting with 13, we generate the following sequence:
+
+-- 13  40  20  10  5  16  8  4  2  1
+-- It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. 
+-- Although it has not been proved yet (Collatz Problem), it is thought that all starting 
+-- numbers finish at 1.
+
+-- Which starting number, under one million, produces the longest chain?
+
+-- NOTE: Once the chain starts the terms are allowed to go above one million.
+
 import Data.List
+import qualified Data.Map as Map
 
-doEven :: (Fractional a, Integral a1) => a1 -> a
-doEven n = fromIntegral(n)/2
+collatzStep :: (Integral a, Integral b) => a -> b
+collatzStep n
+  | odd n     = round((3*fromIntegral(n))+1)
+  | otherwise = round(fromIntegral(n)/2)
 
-doOdd :: (Fractional a, Integral a1) => a1 -> a
-doOdd n = (3*fromIntegral(n))+1
+collatzLength n table
+  | n == 1    = 1
+  | otherwise = (+) 1 $ (collatzLookup . collatzStep) n table
 
-collatzStep :: (Fractional a, Integral a1) => a1 -> a
-collatzStep n = if odd n then doOdd(n) else doEven(n)
 
-doCollatz :: (Integral a) => [a] -> [a]
-doCollatz l@(x:xs)
-  | x == 1    = reverse l
-  | otherwise = doCollatz $ (round $ collatzStep x) : l
+collatzLookup n table = case Map.lookup n table of
+  Just a  -> a
+  Nothing -> (collatzLength n table)
 
-collatz :: Integral a => a -> [a]
-collatz n = doCollatz [n]
+collatzTable x =
+  let insrt table v = Map.insert v (collatzLookup v table) table
+  in foldl insrt Map.empty [1 .. x]
 
-allCollatzUnder :: Integral a => a -> [[a]]
-allCollatzUnder n = map collatz [1..n]
+tableValueCompare :: (Num a, Ord a) => (a1, a) -> (a2, a) -> Ordering
+tableValueCompare a b = (snd a) `compare` (snd b)
 
-lengthCompare :: [a] -> [a1] -> Ordering
-lengthCompare a b = length(a) `compare` length(b)
+tableSort = sortBy tableValueCompare
 
-sortByLength :: [[a]] -> [[a]]
-sortByLength = sortBy lengthCompare
+longestUoTo n = fst $ last $ tableSort $ Map.toList $ collatzTable n
 
-longestCollatzUnder :: Integral a => a -> [a]
-longestCollatzUnder n = last $ sortByLength $ allCollatzUnder n
-
-startForLongestCollatzUnder :: Integral a => a -> a
-startForLongestCollatzUnder n = longestCollatzUnder(n) !! 0
-
-answer = startForLongestCollatzUnder 1000000
+answer = longestUoTo 1000000
